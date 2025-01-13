91b1da0015fb314ecae73a86a606ceaa449d8f25
Orca-Gemini23 <cs.yash95@gmail.com>
2025-01-13 11:50:00 +0530

    hey

--- hooks/post-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get the commit message
-COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-
-# Run the Python script to create a YouTrack issue
-python C:\Users\csyas\OneDrive\Documents\code\AutomateYoutrack\demoYoutrack.py "$COMMIT_MESSAGE"
